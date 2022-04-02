288626b494e76cb413e2dfda4ce635e693a88b28
Alex Ross <alxrg@me.com>
2022-04-02 15:20:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import data, { allCategories, productCount, categories, namesAndCategories, catNameArray } from "./data"
 
 function App() {
+
+  console.log(categories)
+  console.log(namesAndCategories)
+  console.log(catNameArray)
+  console.log(allCategories)
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Product List
+        </h1>
       </header>
+        {catNameArray.map(obj => {
+          return(
+            <button>{obj.name}
+            <span> {obj.count}</span>
+            </button>
+          )
+          })
+        }
+        <p>Product Count: {productCount}</p>
+        <p>Category Count: {catNameArray.length}</p>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,66 @@
+import data from "./data.json"; 
+
+const productCount = data.length
+
+const allCategories = data.map(obj => obj.category)
+
+// ABOVE is the same as BELOW
+// const allCategories = data.map(item => {
+//   return item.category
+// })
+
+new Set()
+// Set is smart and will remove the duplicates from your array
+
+const singleCategories = new Set(allCategories)
+// not currently an array, but we need to have the set be an array
+// or you could do Array.from(new Set(allCategories))
+
+const categories = Array.from(singleCategories)
+
+// const uniqueCategories = data.reduce()
+
+
+// Array with objects
+// [ { name: "Toys", count: 5}, { name: "Books", count: 1} ]
+
+// const namesAndCategories = categories.map(name => {
+//   return {
+//     name, 
+//     count: allCategories.filter(category => category === name).length
+//   }
+// })
+
+// const countCategories = allCategories.reduce((category, n) => n )
+
+const namesAndCategories = allCategories.reduce( (countObj, category) => {
+  if (countObj[category]) {
+    countObj[category] += 1;
+  } else {
+    countObj[category] = 1;
+  }
+  
+  return countObj;
+}, {});
+//  { "Toys":5, "Books":2, "Kids":4}
+// namesAndCategories["Toys"]
+// namesAndCategories.Toys => 5
+// Object.keys(namesAndCategories) => [ "Toys", "Books", "Kids"]
+const catNameArray = Object.keys(namesAndCategories).map(cat => {
+  return {
+    name:cat,
+    //namesAndCategories["Toys"] The Key is "Toys" the value is the number
+    count:namesAndCategories[cat]
+  }
+} )
+// [ "Toys", "Books", "Kids"]
+
+export {
+  allCategories,
+  productCount,
+  categories,
+  namesAndCategories,
+  catNameArray
+}
+
+export default data
